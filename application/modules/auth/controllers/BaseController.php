0390e6a88e63ffc41599beaf0d017107747287c9
Eddie Jaoude <eddie@jaoudestudios.com>
2011-03-06 14:44:25 +0000

    Basic directory & file structure for AUTH module

---
+++ application/modules/auth/controllers/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Auth Base Controller
+ *
+ * Base controller to Default Module
+ *
+ * @author          Eddie Jaoude
+ * @package       Auth Module
+ *
+ */
+abstract class Auth_BaseController extends Zend_Controller_Action {
+
+    
+
+}
+?>
