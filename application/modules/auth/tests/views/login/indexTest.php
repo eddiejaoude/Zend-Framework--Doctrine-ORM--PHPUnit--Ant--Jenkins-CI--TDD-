210fb61f1287f4de116e2dec7e8a8a83d8025b00
Eddie Jaoude <eddie@jaoudestudios.com>
2011-03-27 10:15:14 +0100

    Improved login testing to cover successful & various failures

--- application/modules/auth/tests/views/login/indexTest.php
+++ application/modules/auth/tests/views/login/indexTest.php
@@ -40,19 +40,65 @@ class LoginViewTest extends BaseTestCase {
         $this->assertQueryCount('form#Login', 1);
         $this->assertQueryCount('input#email', 1);
         $this->assertQueryCount('input#password', 1);
+
+        $this->assertQueryCount('div.error', 0);
     }
 
     /**
-     * Test login failure
+     * Test login failure incorrect credentials
      *
      * @author 	Eddie Jaoude
      * @param 	null
      * @return 	null
      *
      */
-    public function testLoginFailure()
+    public function testLoginFailureIncorrectCredentials()
     {
+        $this->request->setMethod('POST')
+              ->setPost(array(
+                  'email' => 'test@test.com',
+                  'password' => 'foobar'
+              ));
 
+        $this->dispatch('/auth/login');
+        $this->assertModule('auth');
+        $this->assertController('login');
+        $this->assertAction('index');
+
+        $this->assertQueryCount('form#Login', 1);
+        $this->assertQueryCount('input#email', 1);
+        $this->assertQueryCount('input#password', 1);
+
+        $this->assertQueryCount('div.error', 1);
+    }
+
+    /**
+     * Test login failure validation error
+     *
+     * @author 	Eddie Jaoude
+     * @param 	null
+     * @return 	null
+     *
+     */
+    public function testLoginFailureValidationError()
+    {
+        $this->request->setMethod('POST')
+              ->setPost(array(
+                  'email' => 'test@test',
+                  'password' => ''
+              ));
+
+        $this->dispatch('/auth/login');
+        $this->assertModule('auth');
+        $this->assertController('login');
+        $this->assertAction('index');
+
+        $this->assertQueryCount('form#Login', 1);
+        $this->assertQueryCount('input#email', 1);
+        $this->assertQueryCount('input#password', 1);
+
+        $this->assertQueryCount('dd#email-element ul.errors', 1);
+        $this->assertQueryCount('dd#password-element ul.errors', 1);
     }
 
     /**
@@ -63,9 +109,20 @@ class LoginViewTest extends BaseTestCase {
      * @return 	null
      *
      */
-    public function testLoginSuccessfull()
+    public function testLoginSuccessful()
     {
+        $this->request->setMethod('POST')
+              ->setPost(array(
+                  'email' => 'test@test.com',
+                  'password' => 'test'
+              ));
 
+        $this->dispatch('/auth/login');
+        $this->assertModule('auth');
+        $this->assertController('login');
+        $this->assertAction('index');
+        
+        $this->assertRedirectTo('/auth');
     }
 
 }
