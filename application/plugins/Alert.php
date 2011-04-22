58e59afcdc863efc6d0c4e3ef4836ecbbfe50f71
Eddie Jaoude <eddie@jaoudestudios.com>
2011-04-22 08:26:17 +0100

    #27: Added a skeleton controller plugin to the application layer which is auto loaded. Additional controller plugins can now be easily added to /application/plugins/. If module specific plugin, use same principles and apply to module only, i.e. /application/modules/auth/controllers/plugins/

---
+++ application/plugins/Alert.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Alert plugin
+ *
+ * @author          Eddie Jaoude
+ * @package       Application Module
+ *
+ */
+class Application_Plugin_Alert extends Zend_Controller_Plugin_Abstract
+{
+
+    /**
+     * Pre dispatch
+     *
+     * @author          Eddie Jaoude
+     * @param           Zend_Controller_Request_Abstract $request
+     * @return           void
+     *
+     */
+    public function preDispatch(Zend_Controller_Request_Abstract $request) {
+        
+    }
+
+    /**
+     * Post dispatch
+     *
+     * @author          Eddie Jaoude
+     * @param           Zend_Controller_Request_Abstract $request
+     * @return           void
+     *
+     */
+    public function postDispatch(Zend_Controller_Request_Abstract $request) {
+        
+    }
+
+
+}
