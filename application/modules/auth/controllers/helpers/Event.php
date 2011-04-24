98746df7f419dbed24efc3a510216c2d05336daf
Eddie Jaoude <eddie@jaoudestudios.com>
2011-04-24 16:35:12 +0100

    #32: Custom Controller Action Helper for account events

---
+++ application/modules/auth/controllers/helpers/Event.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Event action helper
+ *
+ *
+ * @author          Eddie Jaoude
+ * @package       Auth Module
+ *
+ */
+class Auth_Controller_Helper_Event extends Zend_Controller_Action_Helper_Abstract
+{
+
+    /**
+     * Record event
+     *
+     * @author          Eddie Jaoude
+     * @param           object $_em db event manager
+     * @param           string $event
+     * @return           void
+     *
+     */
+    public function record($_em, $event) {
+        $account_event = new Auth_Model_AccountEvent;
+        $account_event->setAccount_id(Zend_Auth::getInstance()->getIdentity()->getId());
+        $account_event->setEvent($event);
+        $date = new Zend_Date;
+        $account_event->setCreated_at($date->toString('YYYY-MM-dd HH:mm:ss'));
+        $_em->persist($account_event);
+        $_em->flush();
+    }
+}
