71b94b35b9507a6ca1141e06fb0b84a54d5996e0
Koen Huybrechts <koen@go-online.be>
2011-04-28 13:28:01 +0200

    Added validation if the new password is the same as the current password.

--- library/Custom/Auth/Validate/NotIdentical.php
+++ library/Custom/Auth/Validate/NotIdentical.php
@@ -7,8 +7,18 @@
 
 class Custom_NotIdentical extends Zend_Validate_Identical
 {
+	const Are_Equal = 'areEqual';
+	protected $_messageTemplates = array(
+	    self::Are_Equal => "'%value%' is identical to the current password"
+	);
     public function isValid($value)
     {
-        return !parent::isValid($value);
+        $validation =  !parent::isValid($value);
+        if($validation) {
+        	return true;
+        } else {
+        	$this->_error(self::Are_Equal);
+        	return false;
+        }
     }
 }
