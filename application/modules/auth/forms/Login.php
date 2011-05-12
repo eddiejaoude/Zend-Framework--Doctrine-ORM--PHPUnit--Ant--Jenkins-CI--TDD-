d1d009964ec3dc9db63216342a90e37c64524d90
David Weinraub <david@papayasoft.com>
2011-05-12 19:37:48 +0700

    #47 Use baseUrl() to operate correctly below web root

--- application/modules/auth/forms/Login.php
+++ application/modules/auth/forms/Login.php
@@ -1,8 +1,7 @@
 <?php
+
 /**
- * Auth Login Form
- *
- * Authentication user form
+ * Auth login form
  *
  * @author 			Eddie Jaoude
  * @category   		Real Browser
@@ -12,36 +11,50 @@
  */
 class Auth_Form_Login extends Zend_Form
 {
-	public function __construct($options = null)
-	{
-		parent::__construct($options);
-		$this->setName('Login')
-			 ->setMethod('post')
-                        ->setAttrib('class', 'box')
-			 ->setAction('/auth/login');
-		
-		# Email	
-		$email = new Zend_Form_Element_Text('email');
-		$email->setLabel('Email')
-				->setRequired(true)
-				->addFilter('StripTags')
-				->addFilter('StringTrim')
-				->addFilter('StringToLower')
-				->addValidator('NotEmpty')
-				->addValidator('EmailAddress');
-		# Password
-		$password = new Zend_Form_Element_Password('password');
-		$password->setLabel('Password')
-				->setRequired(true)
-				->addFilter('StripTags')
-				->addFilter('StringTrim')
-				->addValidator('NotEmpty');
-	
-		# Submit
-		$submit = new Zend_Form_Element_Submit('login');
-		
-		# Create
-		$this->addElements(array($email, $password, $submit));
-	}
+
+    public function init()
+    {
+        /*
+         * Some people consider this to be "interface" stuff,
+         * to be done in the view. Personally, I think 'action' and 'method'
+         * can be done here, though the fact that we need the view object
+         * in order to ender the url for the action suggests that it, too, should
+         * be in the view. But 'name' and 'attribs' really are kind of view-ish.
+         *
+         * Still, I like the idea that the view-script is so simple, just render the form.
+         *
+         * @todo To be discussed.
+         */
+        $this->setMethod('post')
+            ->setAction($this->getView()->url(array(
+                    'module' => 'auth',
+                    'controller' => 'login',
+                    'action' => 'index')))
+            ->setAttrib('class', 'box');
+
+        # Email
+        $email = new Zend_Form_Element_Text('email');
+        $email->setLabel('Email')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addFilter('StringToLower')
+            ->addValidator('NotEmpty')
+            ->addValidator('EmailAddress');
+
+        # Password
+        $password = new Zend_Form_Element_Password('password');
+        $password->setLabel('Password')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+
+        # Submit
+        $submit = new Zend_Form_Element_Submit('login');
+
+        # Create
+        $this->addElements(array($email, $password, $submit));
+    }
 }
-?>
+
--- application/modules/auth/forms/PasswordUpdate.php
+++ application/modules/auth/forms/PasswordUpdate.php
@@ -1,56 +1,58 @@
 <?php
+
 /**
- * Auth Password Reset Form
+ * Auth password update form
  * 
  * @author koen Huybrechts
  *
  */
 class Auth_Form_PasswordUpdate extends Zend_Form
 {
-    public function __construct ($options = null)
+
+    public function init()
     {
-    	# Load the custom validator
-    	$this->addElementPrefixPath(
-                'Custom_Auth_Validate',
-                'Custom/Auth/Validate/',
-                'validate'
+        # Load the custom validator
+        $this->addElementPrefixPath(
+            'Custom_Auth_Validate',
+            'Custom/Auth/Validate/',
+            'validate'
         );
-    	
-		parent::__construct($options);
-		$this->setName('Login')
-			 ->setMethod('post')
-                        ->setAttrib('class', 'box')
-			 ->setAction('/auth/password/update');
-		
-		# Current password
-		$currentPassword = new Zend_Form_Element_Password('currentPassword');
-		$currentPassword->setLabel('Current Password')
-				->setRequired(true)
-				->addFilter('StripTags')
-				->addFilter('StringTrim')
-				->addValidator('NotEmpty');
-		
-		# New password
-		$newPassword = new Zend_Form_Element_Password('newPassword');
-		$newPassword->setLabel('New Password')
-				->setRequired(true)
-				->addFilter('StripTags')
-				->addFilter('StringTrim')
-				->addValidator('NotEmpty');
-		
-		# Confirm new password
-		$confirmPassword = new Zend_Form_Element_Password('confirmPassword');
-		$confirmPassword->setLabel('Current Password')
-				->setRequired(true)
-				->addFilter('StripTags')
-				->addFilter('StringTrim')
-				->addValidator('NotEmpty');
-	
-		# Submit
-		$submit = new Zend_Form_Element_Submit('sendPassword', 'Update Password');
-		
-		# Create
-		$this->addElements(array($currentPassword, $newPassword, $confirmPassword, $submit));
+
+        $this->setMethod('post')
+            ->setAction($this->getView()->url(array(
+                    'module' => 'auth',
+                    'controller' => 'password',
+                    'action' => 'update')))
+            ->setAttrib('class', 'box');
+
+        # Current password
+        $currentPassword = new Zend_Form_Element_Password('currentPassword');
+        $currentPassword->setLabel('Current Password')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+
+        # New password
+        $newPassword = new Zend_Form_Element_Password('newPassword');
+        $newPassword->setLabel('New Password')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+
+        # Confirm new password
+        $confirmPassword = new Zend_Form_Element_Password('confirmPassword');
+        $confirmPassword->setLabel('Current Password')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+
+        # Submit
+        $submit = new Zend_Form_Element_Submit('sendPassword', 'Update Password');
+
+        # Create
+        $this->addElements(array($currentPassword, $newPassword, $confirmPassword, $submit));
     }
 }
-?>
