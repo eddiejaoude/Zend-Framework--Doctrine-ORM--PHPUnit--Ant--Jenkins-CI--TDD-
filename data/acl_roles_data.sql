bd099ee5cffabe353fd46e082ff4b4a395260a63
Koen Huybrechts <koen@go-online.be>
2011-06-02 22:37:55 +0200

    Table structures and data for ACL

---
+++ data/acl_roles_data.sql
@@ -0,0 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.5
+-- http://www.phpmyadmin.net
+--
+-- Machine: localhost
+-- Genereertijd: 02 Jun 2011 om 22:36
+-- Serverversie: 5.1.44
+-- PHP-Versie: 5.3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `skeleton`
+--
+
+--
+-- Gegevens worden uitgevoerd voor tabel `acl_roles`
+--
+
+INSERT INTO `acl_roles` VALUES(1, 'admin', 'Administrators');
+INSERT INTO `acl_roles` VALUES(2, 'user', 'Registered visitors');
