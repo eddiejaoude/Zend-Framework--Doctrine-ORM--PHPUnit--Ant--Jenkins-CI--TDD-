ed8a940440b18587b6b1c6067b959dc6cc2b9cc0
Eddie Jaoude <eddie@jaoudestudios.com>
2011-06-05 08:32:47 +0100

    Fixed sql error

--- data/accounts_role_members.sql
+++ data/accounts_role_members.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `accounts_role_members` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `account_id` unsigned int(10) NOT NULL,
-  `role_id` unsigned int(10) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `account_id` int(10) UNSIGNED NOT NULL,
+  `role_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
