dffbd84cfc6bf1ad6eb05adcd3a490b4ac7758ad
Eddie Jaoude <eddie@jaoudestudios.com>
2011-06-05 11:54:49 +0100

    Full example database dump with dummy data added

---
+++ data/dummy_data/_full_example_dump.sql
@@ -0,0 +1,182 @@
+-- MySQL dump 10.13  Distrib 5.1.54, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: skeleton
+-- ------------------------------------------------------
+-- Server version	5.1.54-1ubuntu4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account_events`
+--
+
+DROP TABLE IF EXISTS `account_events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account_events` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account_id` int(10) unsigned NOT NULL,
+  `event` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account_events`
+--
+
+LOCK TABLES `account_events` WRITE;
+/*!40000 ALTER TABLE `account_events` DISABLE KEYS */;
+INSERT INTO `account_events` VALUES (1,1,'logged in','2011-06-05 11:15:50'),(2,1,'logged in','2011-06-05 11:15:50'),(3,1,'logged in','2011-06-05 11:53:04'),(4,1,'logged in','2011-06-05 11:53:04');
+/*!40000 ALTER TABLE `account_events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,'test','test@test.com','d2ba838e6629500eceefd5fabc1916e5acba3e8c2a379234297d7eba45995c7a','2011-06-05 11:14:25'),(3,'admin','admin@test.com','d2ba838e6629500eceefd5fabc1916e5acba3e8c2a379234297d7eba45995c7a','2010-01-01 00:00:00'),(4,'test','test@test.com','d2ba838e6629500eceefd5fabc1916e5acba3e8c2a379234297d7eba45995c7a','2011-06-05 11:53:04');
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `accounts_role_members`
+--
+
+DROP TABLE IF EXISTS `accounts_role_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts_role_members` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `account_id` int(10) NOT NULL,
+  `role_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts_role_members`
+--
+
+LOCK TABLES `accounts_role_members` WRITE;
+/*!40000 ALTER TABLE `accounts_role_members` DISABLE KEYS */;
+INSERT INTO `accounts_role_members` VALUES (1,1,2),(2,1,1);
+/*!40000 ALTER TABLE `accounts_role_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_privileges`
+--
+
+DROP TABLE IF EXISTS `acl_privileges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acl_privileges` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `module` varchar(255) NOT NULL,
+  `controller` varchar(255) NOT NULL,
+  `action` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl_privileges`
+--
+
+LOCK TABLES `acl_privileges` WRITE;
+/*!40000 ALTER TABLE `acl_privileges` DISABLE KEYS */;
+INSERT INTO `acl_privileges` VALUES (1,'auth','account','index','Watch Profile Information'),(2,'auth','account','update','Update Profile Information'),(3,'default','index','index','Homepage');
+/*!40000 ALTER TABLE `acl_privileges` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_role_privileges`
+--
+
+DROP TABLE IF EXISTS `acl_role_privileges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acl_role_privileges` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` int(10) NOT NULL,
+  `privilege_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl_role_privileges`
+--
+
+LOCK TABLES `acl_role_privileges` WRITE;
+/*!40000 ALTER TABLE `acl_role_privileges` DISABLE KEYS */;
+INSERT INTO `acl_role_privileges` VALUES (1,1,1),(2,3,3),(9,2,1);
+/*!40000 ALTER TABLE `acl_role_privileges` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_roles`
+--
+
+DROP TABLE IF EXISTS `acl_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acl_roles` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl_roles`
+--
+
+LOCK TABLES `acl_roles` WRITE;
+/*!40000 ALTER TABLE `acl_roles` DISABLE KEYS */;
+INSERT INTO `acl_roles` VALUES (1,'admin','Administrators'),(2,'user','Registered visitors'),(3,'guest','Anonymous Visitors');
+/*!40000 ALTER TABLE `acl_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-06-05 11:54:16
