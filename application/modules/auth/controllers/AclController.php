36a87378657675da0ec0d1badbf99ffc62f4dbf5
Eddie Jaoude <eddie@jaoudestudios.com>
2011-06-11 10:10:07 +0100

    #70: Roles page displays list of roles

---
+++ application/modules/auth/controllers/AclController.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Auth ACL Controller
+ *
+ *
+ * @author          Eddie Jaoude
+ * @package       Auth Module
+ *
+ */
+class Auth_AclController extends Auth_BaseController
+{
+
+    /**
+     * Initialisation method
+     *
+     * @author          Eddie Jaoude
+     * @param           void
+     * @return           void
+     *
+     */
+    public function init()
+    {
+        parent::init();
+    }
+
+    /**
+     * initiates before any action is dispatched
+     *
+     * @param	void
+     * @return	void
+     */
+    public function preDispatch() {
+        # if the user is not logged in, they can not access secure pages
+        if (!Zend_Auth::getInstance()->hasIdentity()) {
+            # redirect login page
+            $this->_helper->redirector('denied', 'index', 'auth');
+        }
+    }
+
+    /**
+     * post dispatch method
+     *
+     * @author          Eddie Jaoude
+     * @param           void
+     * @return           void
+     *
+     */
+    public function  postDispatch() {
+        parent::postDispatch();
+    }
+
+    /**
+     * default method
+     *
+     * @author          Eddie Jaoude
+     * @param           void
+     * @return           void
+     *
+     */
+    public function indexAction() {
+        
+    }
+
+    /**
+     * role  method
+     *
+     * @author          Eddie Jaoude
+     * @param           void
+     * @return           void
+     *
+     */
+    public function roleAction() {
+        $modelRoles = $this->_em->getRepository('Auth_Model_Role');
+        $roles = $modelRoles->findBy(array());
+
+        $this->view->roles = $roles;
+    }
+
+
+}
