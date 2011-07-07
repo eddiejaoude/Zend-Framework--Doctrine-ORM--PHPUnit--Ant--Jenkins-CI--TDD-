720d9d39afdd9fdaa3cfc093f0f4d5a933520238
Eddie Jaoude <eddie@jaoudestudios.com>
2011-07-07 09:09:18 +0100

    Upgrade Doctrine from v2.0 to v2.1

--- library/Doctrine/DBAL/Schema/Comparator.php
+++ library/Doctrine/DBAL/Schema/Comparator.php
@@ -99,7 +99,7 @@ class Comparator
                 $diff->newSequences[] = $sequence;
             } else {
                 if ($this->diffSequence($sequence, $fromSchema->getSequence($sequenceName))) {
-                    $diff->changedSequences[] = $fromSchema->getSequence($sequenceName);
+                    $diff->changedSequences[] = $toSchema->getSequence($sequenceName);
                 }
             }
         }
@@ -163,6 +163,7 @@ class Comparator
                 $changes++;
             }
         }
+        
         foreach ( $table1Columns as $columnName => $column ) {
             if ( $table2->hasColumn($columnName) ) {
                 $changedProperties = $this->diffColumn( $column, $table2->getColumn($columnName) );
@@ -249,7 +250,7 @@ class Comparator
         foreach ($tableDifferences->addedColumns AS $addedColumnName => $addedColumn) {
             foreach ($tableDifferences->removedColumns AS $removedColumnName => $removedColumn) {
                 if (count($this->diffColumn($addedColumn, $removedColumn)) == 0) {
-                    $renameCandidates[$addedColumn->getName()][] = array($removedColumn, $addedColumn);
+                    $renameCandidates[$addedColumn->getName()][] = array($removedColumn, $addedColumn, $addedColumnName);
                 }
             }
         }
@@ -257,8 +258,10 @@ class Comparator
         foreach ($renameCandidates AS $candidate => $candidateColumns) {
             if (count($candidateColumns) == 1) {
                 list($removedColumn, $addedColumn) = $candidateColumns[0];
+                $removedColumnName = strtolower($removedColumn->getName());
+                $addedColumnName = strtolower($addedColumn->getName());
 
-                $tableDifferences->renamedColumns[$removedColumn->getName()] = $addedColumn;
+                $tableDifferences->renamedColumns[$removedColumnName] = $addedColumn;
                 unset($tableDifferences->addedColumns[$addedColumnName]);
                 unset($tableDifferences->removedColumns[$removedColumnName]);
             }
@@ -332,7 +335,7 @@ class Comparator
         }
 
         if ($column1->getType() instanceof \Doctrine\DBAL\Types\DecimalType) {
-            if ($column1->getPrecision() != $column2->getPrecision()) {
+            if (($column1->getPrecision()?:10) != ($column2->getPrecision()?:10)) {
                 $changedProperties[] = 'precision';
             }
             if ($column1->getScale() != $column2->getScale()) {
@@ -342,6 +345,11 @@ class Comparator
 
         if ($column1->getAutoincrement() != $column2->getAutoincrement()) {
             $changedProperties[] = 'autoincrement';
+        }
+
+        // only allow to delete comment if its set to '' not to null.
+        if ($column1->getComment() !== null && $column1->getComment() != $column2->getComment()) {
+            $changedProperties[] = 'comment';
         }
 
         return $changedProperties;
