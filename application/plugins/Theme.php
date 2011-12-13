5d85debf28c6b005dbc9ba8df2bd4a91429c2522
Eddie Jaoude <eddie@jaoudestudios.com>
2011-12-13 07:18:51 +0000

    #89: Removed debug & added a few comments to pull request

--- application/plugins/Theme.php
+++ application/plugins/Theme.php
@@ -19,19 +19,19 @@ class Application_Plugin_Theme extends Zend_Controller_Plugin_Abstract
      */
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
-        // echo "<pre>" . var_dump($request) . "</pre>";
-        // die(__FILE__ . "(" . __LINE__ . ") :: " . __FUNCTION__ . " :: message");
-
-
+        # check for authenticated user
         $auth = Zend_Auth::getInstance();
         if (!$auth->hasIdentity()){
             return;
         }
+        
+        # get user details
         $user = $auth->getIdentity();
         $theme = $user->getTheme();
+        
+        # set theme & send to view
         $this->setLayout($theme);
         $this->addFormToView($theme);
-
     }
 
     /**
@@ -63,12 +63,15 @@ class Application_Plugin_Theme extends Zend_Controller_Plugin_Abstract
      */
     protected function addFormToView($theme)
     {
+        # get theme form
         $form = new Auth_Form_ThemeSelect();
         
         $form->setDefaults(array(
             'theme' => $theme,
             'redirectUrl' => $_SERVER['REQUEST_URI'],
         ));
+        
+        # send to view
         Zend_Controller_Action_HelperBroker::getExistingHelper('ViewRenderer')->view->formThemeSelect = $form;        
     }
 }
